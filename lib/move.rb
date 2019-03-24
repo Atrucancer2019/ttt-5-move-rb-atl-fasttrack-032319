a3d39347beac0a84615cafac2844d3cc254a03ea
sagacious-flow-6478 <jacobydickson09@aol.com>
2019-03-24 19:00:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(choice)
+    choice.to_i - 1
+end
+
+def move(board, index, piece = "X")
+    puts "player wants to move to #(index)"
+    board[index] = piece
+end
